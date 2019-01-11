c7c6bc211be6f1c6de538833d3fa26a69b8c015e
Mikolaj Kaczmarek <extern.mikolaj.kaczmarek@elektrobit.com>
2019-01-11 11:04:35 +0100

    fixed problem with build type

--- benchmark/bench/src/bench.cpp
+++ benchmark/bench/src/bench.cpp
@@ -3,7 +3,9 @@
 
 static void BM_StringCreation(benchmark::State& state)
 {
-    for (auto _ : state) std::string empty{create_empty_string()};
+    for (auto _ : state) {
+        for (int i = 0; i < 1E3; ++i) std::string empty{create_empty_string()};
+    }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_StringCreation);
@@ -12,7 +14,9 @@ BENCHMARK(BM_StringCreation);
 static void BM_StringCopy(benchmark::State& state)
 {
     std::string x = "hello";
-    for (auto _ : state) std::string copied{copy_string(x)};
+    for (auto _ : state) {
+        for (int i = 0; i < 1E3; ++i) std::string copied{copy_string(x)};
+    }
 }
 BENCHMARK(BM_StringCopy);
 
--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+# Run this script with build type as a parameter (Release or Debug)
+# example:
+# ./run_benchmark Release
+# if parameter is not given, build type is set to Release
+
+if [ "$1" == "Release" ] || [ -z "$1" ]; then
+    build_type="Release"
+elif [ "$1" == "Debug" ]; then
+    build_type="Debug"
+else
+    echo "Wrong build type. Use 'Release' or 'Debug'"
+    exit 1
+fi
+echo "Chosen build type: ${build_type}"
 cd ./build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE
+cmake .. -DCMAKE_BUILD_TYPE="${build_type}"
 make -j8
 ./bench/bench
